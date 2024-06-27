93ab592aa98155d66553524a1329fb525654dbab
OlegVasilevU <trade.vasilev@yandex.ru>
2024-06-27 21:25:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-print('Hello my friend!')
-print(len('Hello my friend!'))
-12
-9
-print(12 + 9)
-print(12 - 9)
-12
-9
-31
-print((12 + 9 +31) / 3)
-'Вторник'
-'Понедельник'
-print('Понедельник, ' + 'Вторник')
-13
-15
-18
-print((((13 * 15) + (13 * 18)) ** 3) / 2)
+# 1st program
+a = 'Hello_my_friend'
+print(len(a))
+# 2st program
+first = 12
+second = 9
+summa = (int(first + second))
+diff = (int(first - second))
+print(summa, diff)
+# 3st program
+first = 12
+second = 9
+third = 31
+mean = (float((first + second + third) / 3))
+print(mean)
+# 4st program
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(str(second_string + ', ' + first_string))
+# 5st program
+a = 13
+b = 15
+c = 18
+f = (a * b) + (a * c)
+print(int(f **3) /2)
+
